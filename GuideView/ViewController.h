10f681c754bc783bc214a67a3ed13fa0d5ec0701
ZunHoko <tianchaojiang@TianchaodeiMac.local>
2017-03-11 15:35:41 +0800

    Create

---
+++ GuideView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuideView
+//
+//  Created by ZHK on 2017/3/11.
+//  Copyright © 2017年 Weiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GuideView/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  GuideView
+//
+//  Created by ZHK on 2017/3/11.
+//  Copyright © 2017年 Weiyu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZHKGuideView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    if (![ZHKGuideView needShowGuide]) {
+        ZHKGuideView *guideView = [ZHKGuideView guideViewWithImageNames:@[@"guide_image_01", @"guide_image_02", @"guide_image_03", @"guide_image_04"]];
+        [guideView showGuideViewOnTargetController:self.navigationController];
+    }
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
